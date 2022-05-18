e2ae963297a4075f2dd8f0aade6ec2eb6d3ce7c9
Your Name <lukman.webdev@gmail.com>
2022-05-18 22:13:55 +0600

    admin added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Cart from "./components/UserSite/Cart/Cart";
 import Details from "./components/UserSite/Details/Details";
 import Shiping from "./components/UserSite/Shiping/Shiping";
 import Information from "./components/UserSite/Information/Information";
-
-
 import Login from "./components/Login/Login/Login";
 import Register from "./components/Login/Register/Register";
 import NavBar from "./components/Shared/NavBar/NavBar";
 import AuthProvider from "./contexts/AuthProvider/AuthProvider";
+import PrivateRoute from "./components/Login/PrivateRoute/PrivateRoute";
+import HomeProduct from "./components/UserSite/HomeProduct/HomeProduct";
+import MakeAdmin from "./components/Admin/MakeAdmin/MakeAdmin";
+import AdminRoute from "./components/Login/AdminRoute/AdminRoute";
 
 function App() {
   return (
@@ -24,9 +26,15 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/AddProduct" element={<AddProduct />} />
+            <Route path="/product" element={<HomeProduct />} />
+            <Route path="/" element={<AdminRoute />}>
+              <Route path="/makeadmin" element={<MakeAdmin />} />
+            </Route>
             <Route path="/cart" element={<Cart />} />
             <Route path="/details/:id" element={<Details />} />
-            <Route path="/Shiping" element={<Shiping />} />
+            <Route path="/" element={<PrivateRoute />}>
+              <Route path="/Shiping" element={<Shiping />} />
+            </Route>
             <Route path="/Information" element={<Information />} />
           </Routes>
         </Router>
@@ -38,4 +46,4 @@ function App() {
 export default App;
 <Route path="/cart">
   <Cart />
-</Route>
+</Route>;
