b4fde91d586067ffc729c7573fc3feb603bfb88b
Mehedi hasan <mehediph2021@gmail.com>
2022-05-22 00:17:30 +0600

    delete method

---
+++ src/components/Admin/ManageProduct/ManageProduct.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import ProductData from './ProductData';
+import './ManageProduct.css'
+
+const ManageProduct = () => {
+
+    const [ManageProduct, setManageProduct] = useState([])
+
+    const [lodding, setlodding] = useState(false)
+
+    useEffect(() => {
+        fetch("http://localhost:5000/Product")
+            .then(res => res.json())
+            .then(data => setManageProduct(data))
+    }, [lodding])
+
+
+
+    return (
+        <div className='container'>
+            <Table responsive="sm md xl">
+                <thead>
+                    <tr>
+                        <th>Sl.No</th>
+                        <th>Product</th>
+                        <th>Product-Name</th>
+                        <th>Catagory</th>
+                        <th>Model</th>
+                        <th>Brand_Name</th>
+                        <th>price</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        ManageProduct.map((Product, index) => <ProductData
+                            key={Product._id}
+                            Product={Product}
+                            index={index}
+                            setlodding={setlodding}
+                            ManageProduct={ManageProduct}
+                            setManageProduct={setManageProduct}
+                        ></ProductData>)
+                    }
+                </tbody>
+            </Table>
+
+        </div>
+    );
+};
+
+export default ManageProduct;
---
+++ src/components/Admin/ManageProduct/ProductData.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import axios from 'axios';
+
+
+const ProductData = ({ Product, index, setlodding, setManageProduct, ManageProduct }) => {
+
+    const { _id, Brand_Name, Catagory, Model, Product_Name, image, price } = Product;
+    // .....................................Manage product Delete And update Work Start ......................
+    const handeldelet = (id) => {
+        const prosid = window.confirm('are you sure delete')
+        if (prosid) {
+            setlodding(true)
+            axios.delete(`http://localhost:5000/Product/${id}`)
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        const remainguser = ManageProduct?.filter(manages => manages._id !== id)
+                        setManageProduct(remainguser)
+                        setlodding(false)
+                    }
+                })
+        }
+    }
+    // .....................................Manage product Delete And update Work End ......................
+    return (
+
+        <tr>
+            <td>{index + 1}</td>
+            <td><img src={image} alt="" className='manageproduct-image' /></td>
+            <td>{Product_Name}</td>
+            <td>{Catagory}</td>
+            <td>{Model}</td>
+            <td>{Brand_Name}</td>
+            <td>{price}</td>
+            <td className='status-change-btn'>
+                <DropdownButton
+                    variant="outline-secondary"
+                    id="input-group-dropdown-1"
+                    className='dropbutton'
+                >
+                    <Dropdown.Item>
+                        <button
+                            onClick={() => handeldelet(_id)}
+                            className='status'>
+                            Delete
+                        </button>
+                    </Dropdown.Item>
+                    <Dropdown.Item >
+                        <button
+                            // onClick={() => handleDelivery(_id)}
+                            className='status'>Edit</button>
+                    </Dropdown.Item>
+
+
+                </DropdownButton>
+
+
+
+
+            </td>
+        </tr>
+
+
+    );
+};
+
+export default ProductData;
